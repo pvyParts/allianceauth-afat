26e6c53872e3623a09a293e088e4d6c96791cab9
Peter Pfeufer <info@ppfeufer.de>
2020-09-16 19:28:46 +0200

    fixed minimum AA version requied django-esi v2 related

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 install_requires = [
     "django-bootstrap-form",
-    "allianceauth>=2.7.2",
+    "allianceauth>=2.7.3",
 ]
 
 setup(
