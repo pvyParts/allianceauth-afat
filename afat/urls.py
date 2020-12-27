3d144ceb395fb00f77285471ddfb84bdd8252d0e
Peter Pfeufer <info@ppfeufer.de>
2020-12-27 11:16:19 +0100

    urls changed to be more consistent with their actual purpose

--- afat/urls.py
+++ afat/urls.py
@@ -14,94 +14,96 @@ urlpatterns = [
     # dashboard
     url(r"^$", views.dashboard.dashboard, name="dashboard"),
     # stats main page
-    url(r"^statistic/$", views.statistics.stats, name="stats"),
-    url(r"^statistic/(?P<year>[0-9]+)/$", views.statistics.stats, name="stats"),
+    url(r"^statistics/$", views.statistics.stats, name="stats"),
+    url(r"^statistics/(?P<year>[0-9]+)/$", views.statistics.stats, name="stats"),
     # stats corp
-    url(r"^statistic/corporation/$", views.statistics.stats_corp, name="stats_corp"),
+    url(r"^statistics/corporation/$", views.statistics.stats_corp, name="stats_corp"),
     url(
-        r"^statistic/corporation/(?P<corpid>[0-9]+)/$",
+        r"^statistics/corporation/(?P<corpid>[0-9]+)/$",
         views.statistics.stats_corp,
         name="stats_corp",
     ),
     url(
-        r"^statistic/corporation/(?P<corpid>[0-9]+)/(?P<year>[0-9]+)/$",
+        r"^statistics/corporation/(?P<corpid>[0-9]+)/(?P<year>[0-9]+)/$",
         views.statistics.stats_corp,
         name="stats_corp",
     ),
     url(
-        r"^statistic/corporation/(?P<corpid>[0-9]+)/(?P<year>[0-9]+)/(?P<month>[0-9]+)/$",
+        r"^statistics/corporation/(?P<corpid>[0-9]+)/(?P<year>[0-9]+)/(?P<month>[0-9]+)/$",
         views.statistics.stats_corp,
         name="stats_corp",
     ),
     # stats char
-    url(r"^statistic/character/$", views.statistics.stats_char, name="stats_char"),
+    url(r"^statistics/character/$", views.statistics.stats_char, name="stats_char"),
     url(
-        r"^statistic/character/(?P<charid>[0-9]+)/$",
+        r"^statistics/character/(?P<charid>[0-9]+)/$",
         views.statistics.stats_char,
         name="stats_char",
     ),
     url(
-        r"^statistic/character/(?P<charid>[0-9]+)/(?P<year>[0-9]+)/(?P<month>[0-9]+)/$",
+        r"^statistics/character/(?P<charid>[0-9]+)/(?P<year>[0-9]+)/(?P<month>[0-9]+)/$",
         views.statistics.stats_char,
         name="stats_char",
     ),
     # stats alliance
-    url(r"^statistic/alliance/$", views.statistics.stats_alliance, name="stats_ally"),
+    url(r"^statistics/alliance/$", views.statistics.stats_alliance, name="stats_ally"),
     url(
-        r"^statistic/alliance/(?P<allianceid>[0-9]+)/$",
+        r"^statistics/alliance/(?P<allianceid>[0-9]+)/$",
         views.statistics.stats_alliance,
         name="stats_ally",
     ),
     url(
-        r"^statistic/alliance/(?P<allianceid>[0-9]+)/(?P<year>[0-9]+)/$",
+        r"^statistics/alliance/(?P<allianceid>[0-9]+)/(?P<year>[0-9]+)/$",
         views.statistics.stats_alliance,
         name="stats_ally",
     ),
     url(
-        r"^statistic/alliance/(?P<allianceid>[0-9]+)/(?P<year>[0-9]+)/(?P<month>[0-9]+)/$",
+        r"^statistics/alliance/(?P<allianceid>[0-9]+)/(?P<year>[0-9]+)/(?P<month>[0-9]+)/$",
         views.statistics.stats_alliance,
         name="stats_ally",
     ),
-    # fat links
-    url(r"^links/$", views.fatlinks.links, name="links"),
-    url(r"^links/(?P<year>[0-9]+)/$", views.fatlinks.links, name="links"),
+    # fatlinks
+    url(r"^fatlinks/$", views.fatlinks.links, name="links"),
+    url(r"^fatlinks/(?P<year>[0-9]+)/$", views.fatlinks.links, name="links"),
     url(
-        r"^links/create/esi/(?P<fatlink_hash>[a-zA-Z0-9]+)/$",
+        r"^fatlinks/create/esi/(?P<fatlink_hash>[a-zA-Z0-9]+)/$",
         views.fatlinks.link_create_esi,
         name="link_create_esi",
     ),
     url(
-        r"^links/create/esifat/$", views.fatlinks.create_esi_fat, name="create_esi_fat"
+        r"^fatlinks/create/esifat/$",
+        views.fatlinks.create_esi_fat,
+        name="create_esi_fat",
     ),
     url(
-        r"^links/create/click/$",
+        r"^fatlinks/create/click/$",
         views.fatlinks.link_create_click,
         name="link_create_click",
     ),
-    url(r"^links/add/$", views.fatlinks.link_add, name="link_add"),
-    url(r"^links/edit/$", views.fatlinks.link_edit, name="link_edit"),
+    url(r"^fatlinks/add/$", views.fatlinks.link_add, name="link_add"),
+    url(r"^fatlinks/edit/$", views.fatlinks.link_edit, name="link_edit"),
     url(
-        r"^links/(?P<fatlink_hash>[a-zA-Z0-9]+)/edit/$",
+        r"^fatlinks/(?P<fatlink_hash>[a-zA-Z0-9]+)/edit/$",
         views.fatlinks.link_edit,
         name="link_edit",
     ),
     url(
-        r"^links/(?P<fatlink_hash>[a-zA-Z0-9]+)/click/$",
+        r"^fatlinks/(?P<fatlink_hash>[a-zA-Z0-9]+)/click/$",
         views.fatlinks.click_link,
         name="link_click",
     ),
-    url(r"^links/del/$", views.fatlinks.del_link, name="link_delete"),
+    url(r"^fatlinks/del/$", views.fatlinks.del_link, name="link_delete"),
     url(
-        r"^links/(?P<fatlink_hash>[a-zA-Z0-9]+)/del/$",
+        r"^fatlinks/(?P<fatlink_hash>[a-zA-Z0-9]+)/del/$",
         views.fatlinks.del_link,
         name="link_delete",
     ),
     url(
-        r"^links/(?P<fatlink_hash>[a-zA-Z0-9]+)/(?P<fat>[0-9]+)/del/$",
+        r"^fatlinks/(?P<fatlink_hash>[a-zA-Z0-9]+)/(?P<fat>[0-9]+)/del/$",
         views.fatlinks.del_fat,
         name="fat_delete",
     ),
-    # ajax calls
+    # ajax calls :: dashboard
     url(
         r"^ajax/dashboard/get_fats/(?P<charid>[0-9]+)/$",
         views.dashboard.dashboard_fats_data,
@@ -112,14 +114,15 @@ urlpatterns = [
         views.dashboard.dashboard_links_data,
         name="dashboard_links_data",
     ),
-    url(r"^ajax/links/get_fatlinks/$", views.fatlinks.links_data, name="links_data"),
+    # ajax calls :: fatlinks
+    url(r"^ajax/fatlinks/get_fatlinks/$", views.fatlinks.links_data, name="links_data"),
     url(
-        r"^ajax/links/get_fatlinks/(?P<year>[0-9]+)/$",
+        r"^ajax/fatlinks/get_fatlinks/(?P<year>[0-9]+)/$",
         views.fatlinks.links_data,
         name="links_data",
     ),
     url(
-        r"^ajax/links/(?P<fatlink_hash>[a-zA-Z0-9]+)/edit/$",
+        r"^ajax/fatlinks/(?P<fatlink_hash>[a-zA-Z0-9]+)/edit/$",
         views.fatlinks.link_edit_fat_data,
         name="link_edit_fat_data",
     ),
