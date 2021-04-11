8746acb6dc0ce9074a3e1a8efcbbba6805e3f471
Peter Pfeufer <info@ppfeufer.de>
2021-04-11 17:23:40 +0200

    bring function notation in line with what we already have

--- afat/static/afat/javascript/allianceauth-afat.js
+++ afat/static/afat/javascript/allianceauth-afat.js
@@ -1,20 +1,20 @@
 /**
  * convert a string to a slug
- * @param text
+ * @param {string} text
  * @returns {string}
  */
-function convertStringToSlug (text) {
+let convertStringToSlug = function (text) {
     'use strict';
 
     return text.toLowerCase().replace(/[^\w ]+/g, '').replace(/ +/g, '-');
-}
+};
 
 /**
  * sorting a table by its first columns alphabetically
- * @param table
- * @param order
+ * @param {element} table
+ * @param {string} order
  */
-function sortTable (table, order) {
+let sortTable = function (table, order) {
     'use strict';
 
     let asc = order === 'asc';
@@ -27,14 +27,14 @@ function sortTable (table, order) {
             return $('td:first', b).text().localeCompare($('td:first', a).text());
         }
     }).appendTo(tbody);
-}
+};
 
 /**
  * manage a modal window
  * @param {element} modalElement
  * @param {string} modalBodyText
  */
-function manageModal (modalElement, modalBodyText) {
+let manageModal = function (modalElement, modalBodyText) {
     'use strict';
 
     modalElement.on('show.bs.modal', function (event) {
@@ -45,10 +45,7 @@ function manageModal (modalElement, modalBodyText) {
 
         modal.find('#confirm-action').attr('href', url);
         modal.find('.modal-body').text(
-            modalBodyText.replace(
-                '##NAME##',
-                name
-            )
+            modalBodyText.replace('##NAME##', name)
         );
     }).on('hide.bs.modal', function () {
         let modal = $(this);
@@ -56,4 +53,4 @@ function manageModal (modalElement, modalBodyText) {
         modal.find('.modal-body').html('');
         modal.find('#confirm-action').attr('href', '');
     });
-}
+};
